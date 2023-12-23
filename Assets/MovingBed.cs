ae2ca1826e8cf8661c294ac2860074076afed0d9
Mateo Paladin <mp52512@fer.hr>
2023-12-23 12:20:34 +0100

    Krevet popravljen

--- Assets/MovingBed.cs
+++ Assets/MovingBed.cs
@@ -7,6 +7,7 @@ using UnityEngine.UIElements;
 public class MovingBed : MonoBehaviour
 {
     [SerializeField] GameObject _newTask;
+    [SerializeField] GameObject _oldPic;
     private bool _bedmoved = false;
     // Start is called before the first frame update
     void Start()
@@ -17,15 +18,17 @@ public class MovingBed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (_bedmoved) { 
             float step = 2 * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, -0.819999993f, 0.920000017f), step);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 0.819999993f, 0.920000017f), step);
         }
     }
 
     public void MoveBed() {
         _bedmoved = true;
-        transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(transform.rotation.x - 90f, transform.rotation.y, transform.rotation.z, 1), Time.deltaTime * 2);
+        transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(transform.rotation.x -0.22f, transform.rotation.y, transform.rotation.z, 1),1);
         _newTask.SetActive(true);
+        _oldPic.SetActive(false);
     }
 }
